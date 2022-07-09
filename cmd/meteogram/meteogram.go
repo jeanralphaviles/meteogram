5fae9ea0f5127579625073335c2f8102a38badab
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2022-07-08 23:49:38 -0700

    meteogram: Default to listen on $PORT environment variable set by App Engine.
    We have been seeing this error message in the Meteogram App Engine logs:
    
      App is listening on port 8080. We recommend your app listen on the
      port defined by the PORT environment variable to take advantage of an
      NGINX layer on port 8080.

--- cmd/meteogram/meteogram.go
+++ cmd/meteogram/meteogram.go
@@ -20,6 +20,13 @@ func main() {
 	r.HandleFunc("/", h.Readme).Methods("GET")
 	r.HandleFunc("/meteogram/{latitude},{longitude}", h.Meteogram).Methods("GET")
 	h := handlers.LoggingHandler(os.Stdout, r)
+
+	// App Engine recommends that we listen on the port defined by the PORT variable.
+	if env, ok := os.LookupEnv("PORT"); ok {
+		log.Printf("Overriding --port=%s with PORT=%s environment variable.", *port, env)
+		*port = ":" + env
+	}
+
 	log.Printf("Starting HTTP server listening on %s...", *port)
 	log.Fatal(http.ListenAndServe(*port, h))
 }
